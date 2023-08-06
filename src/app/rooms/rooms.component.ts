654396677461a89e74369a0cdcf4f52006693142
Herbert Owusu <77298594+herbs61@users.noreply.github.com>
2023-08-06 17:13:53 +0000

    Adding some styles and pipes to it

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -14,12 +14,42 @@ export class RoomsComponent {
     hideRooms = false;
 
     rooms : Room = {
+       totalRooms: 20,
+      availableRooms: 10,
+      bookedRooms: 5
+    };
 
+    roomList: RoomList[] = [{
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:'https://images.unspaced.com/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos:'https://images.unspaced.com/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 15000,
+      photos:'https://images.unspaced.com/',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
     }
 
-    // totalRooms: 20,
-    //   availableRooms: 10,
-    //   bookedRooms: 5
+  ];
+
+
     toggle(){
 this.hideRooms = !this.hideRooms;
     }
